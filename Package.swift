4f2692ff411add1207df7180b412c71891c95fec
gogolook-ios <gogolook-ios@gogolook.com>
2025-09-09 10:26:08 +0000

    Bump the version to 0.3.7

--- Package.swift
+++ Package.swift
@@ -10,12 +10,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SafeConnection",
-            url: "https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.3.6/SafeConnection.xcframework.zip",
-            checksum: "f67e9dec686ce10559c80027d077e2bdbf4bd1bca9eccf14afe659a1f3119ccc"
+            url: "https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.3.7/SafeConnection.xcframework.zip",
+            checksum: "fab74c8d74ed9e87fe1188def3b30566c24de5527c60d8c796f3de19c0861d9d"
         ),
     ],
     dependencies: [
-	.package(url: "https://github.com/Moya/Moya", upToNextMajor(from: "15.0.0")),
-        .package(url: "https://github.com/realm/realm-swift.git", exact:("10.54.5")),
+	.package(url: "https://github.com/Moya/Moya", .upToNextMajor(from: "15.0.0")),
+        .package(url: "https://github.com/realm/realm-swift.git", .exact:("10.54.5")),
     ],
 )
--- README.md
+++ README.md
@@ -21,7 +21,7 @@ import SafeConnection
 // ...
 // Add SafeConnection as a dependency
 dependencies: [
-    .package(url: "https://github.com/Gogolook-Inc/safe-connection-ios-sdk", .upToNextMajor(from: "0.3.6"))
+    .package(url: "https://github.com/Gogolook-Inc/safe-connection-ios-sdk", .upToNextMajor(from: "0.3.7"))
 ]
 // ...
 // Add SafeConnection to your target
@@ -29,7 +29,7 @@ dependencies: [
 ```
 ### CocoaPods
 ``` ruby
-pod "SafeConnection", :git => "https://github.com/Gogolook-Inc/safe-connection-ios-sdk", :tag => "0.3.6"
+pod "SafeConnection", :git => "https://github.com/Gogolook-Inc/safe-connection-ios-sdk", :tag => "0.3.7"
 ```
 
 ## Required
