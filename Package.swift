2ead3321945b13e665edcdeb052e6f2b4c61ba19
gogolook-ios <gogolook-ios@gogolook.com>
2025-09-09 16:04:00 +0000

    Bump the version to 0.3.9

--- Package.swift
+++ Package.swift
@@ -7,15 +7,15 @@ let package = Package(
     products: [
         .library(name: "SafeConnection", targets: ["SafeConnection"]),
     ],
+    dependencies: [
+	.package(url: "https://github.com/Moya/Moya", .upToNextMajor(from: "15.0.0")),
+        .package(url: "https://github.com/realm/realm-swift.git", .exact: "10.54.5"),
+    ],
     targets: [
         .binaryTarget(
             name: "SafeConnection",
-            url: "https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.3.7/SafeConnection.xcframework.zip",
-            checksum: "fab74c8d74ed9e87fe1188def3b30566c24de5527c60d8c796f3de19c0861d9d"
+            url: "https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.3.9/SafeConnection.xcframework.zip",
+            checksum: "e0c3b4a4fa33926ffe4837df8ef6ecef47e954976eeac1a3bec0bd105b85181c"
         ),
-    ],
-    dependencies: [
-	.package(url: "https://github.com/Moya/Moya", .upToNextMajor(from: "15.0.0")),
-        .package(url: "https://github.com/realm/realm-swift.git", .exact:("10.54.5")),
     ],
 )
