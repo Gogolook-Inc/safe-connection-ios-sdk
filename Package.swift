f5d502f4e9e261c693f483e0772ebd7558456f25
gogolook-ios <gogolook-ios@gogolook.com>
2025-09-13 02:16:19 +0000

    Bump the version to 0.5.9

--- Package.swift
+++ Package.swift
@@ -13,15 +13,15 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Moya/Moya", from: "15.0.3"),
         .package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "4.1.3"),
-        .package(url: "https://github.com/realm/realm-swift.git", exact: "10.54.5"),
+        // .package(url: "https://github.com/realm/realm-swift.git", exact: "10.54.5"), // Removed for testing
 //      .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.58.2"),
         .package(url: "https://github.com/ZipArchive/ZipArchive", from: "2.6.0"),
     ],
     targets: [
         .binaryTarget(
             name: "SafeConnectionBinary",
-            url: "https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.5.8/SafeConnection.xcframework.zip",
-            checksum: "8e18a4b8dbe95665fa22f5d7bd41d5ec24d7bd7399637cdbdeb600acb63cf910"
+            url: "https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.5.9/SafeConnection.xcframework.zip",
+            checksum: "e0c839c66de6fc94d0ab4af55f591301dcd21b87ad5171febe24616a41949bfc"
         ),
         .target(
             name: "SafeConnection",
@@ -29,7 +29,7 @@ let package = Package(
                 .target(name: "SafeConnectionBinary"),
                 .product(name: "Moya", package: "Moya"),
                 .product(name: "PhoneNumberKit-Dynamic", package: "PhoneNumberKit"),
-                .product(name: "RealmSwift", package: "realm-swift"),
+                // .product(name: "RealmSwift", package: "realm-swift"), // Removed for testing
                 .product(name: "ZipArchive", package: "ZipArchive"),
             ],
         )
--- SafeConnection.podspec
+++ SafeConnection.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "SafeConnection"
-  s.version      = "0.5.8"
+  s.version      = "0.5.9"
   s.summary      = "SafeConnection"
   s.description  = "SafeConnection iOS SDK"
   s.homepage     = "https://www.gogolook.com/"
@@ -9,17 +9,17 @@ Pod::Spec.new do |s|
 
   s.ios.deployment_target = '15.5'
 
-  s.source = { :http => 'https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.5.8/SafeConnection.xcframework.zip' }
+  s.source = { :http => 'https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.5.9/SafeConnection.xcframework.zip' }
 
   s.vendored_frameworks = 'SafeConnection.xcframework'
 
   s.dependency 'Moya', '>= 15.0.3'
   s.dependency 'PhoneNumberKit/PhoneNumberKitCore', '>= 4.1.3'
-  s.dependency 'RealmSwift', '10.54.5'
+  # s.dependency 'RealmSwift', '10.54.5' # Removed for testing
   s.dependency 'SSZipArchive', '>= 2.6.0'
 
   s.prepare_command = <<-CMD
-    curl -L -o SafeConnection.xcframework.zip 'https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.5.8/SafeConnection.xcframework.zip'
+    curl -L -o SafeConnection.xcframework.zip 'https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.5.9/SafeConnection.xcframework.zip'
     unzip SafeConnection.xcframework.zip
     rm -f SafeConnection.xcframework.zip
   CMD
