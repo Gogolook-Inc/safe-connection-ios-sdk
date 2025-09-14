7bae82e720bbd83223cc1bdc05f6906e3ea22ac7
gogolook-ios <gogolook-ios@gogolook.com>
2025-09-14 04:13:40 +0000

    Bump the version to 0.5.13

--- Package.swift
+++ Package.swift
@@ -11,26 +11,26 @@ let package = Package(
         .library(name: "SafeConnection", targets: ["SafeConnection_Aggregation"]),
     ],
     dependencies: [
-//      .package(url: "https://github.com/Moya/Moya", from: "15.0.3"),
-//      .package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "4.1.3"),
+        .package(url: "https://github.com/Moya/Moya", exact: "15.0.3"),
+        .package(url: "https://github.com/marmelroy/PhoneNumberKit", exact: "4.1.3"),
 //      .package(url: "https://github.com/realm/realm-swift.git", exact: "10.54.5"), // Removed for testing
-//      .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.58.2"),
-//       .package(url: "https://github.com/ZipArchive/ZipArchive", from: "2.6.0"),
+//      .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.58.2"),
+        .package(url: "https://github.com/ZipArchive/ZipArchive", exact: "2.6.0"),
     ],
     targets: [
         .binaryTarget(
             name: "SafeConnection",
-            url: "https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.5.12/SafeConnection.xcframework.zip",
-            checksum: "b6ddcab7c23fa4b57323cbb2773dc3d18127d7b77cda440fb234dd2a3e505c68"
+            url: "https://github.com/Gogolook-Inc/safe-connection-ios-sdk/releases/download/0.5.13/SafeConnection.xcframework.zip",
+            checksum: "c513dc560efa017069cc8e33e02976aa2c629cb5440cf62ecf375fe65a6f3d7d"
         ),
         .target(
             name: "SafeConnection_Aggregation",
             dependencies: [
                 .target(name: "SafeConnection"),
-                //.product(name: "Moya", package: "Moya"),
-                //.product(name: "PhoneNumberKit", package: "PhoneNumberKit"),
-                // .product(name: "RealmSwift", package: "realm-swift"), // Removed for testing
-                //.product(name: "ZipArchive", package: "ZipArchive"),
+                .product(name: "Moya", package: "Moya"),
+                .product(name: "PhoneNumberKit-Dynamic", package: "PhoneNumberKit"),
+//              .product(name: "RealmSwift", package: "realm-swift"), // Removed for testing
+                .product(name: "ZipArchive", package: "ZipArchive"),
             ],
         )
     ]
